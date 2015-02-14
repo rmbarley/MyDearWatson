ede850b8cb97be653b49600ca8d0ab37f4e98eb7
Ryan <rmbarley@gmail.com>
2015-02-13 21:46:29 -0500

    Update watson.sh

--- watson.sh
+++ watson.sh
@@ -20,7 +20,7 @@ chroot () {
 }
 
 chrome () {
-	sh -e crouton -t xfce,keyboard,extension
+	sh -e crouton -t xiwi,xfce,keyboard,extension
 	sudo enter-chroot -u root sh ~/Downloads/watson.sh c
 	cd /usr/local/bin
 	sudo cp startxfce4 startelementary
